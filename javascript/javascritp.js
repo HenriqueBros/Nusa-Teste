15234925b131e55ec44f9c3af7081db7e89f03f2
HenriqueBros <58406947+HenriqueBros@users.noreply.github.com>
2020-09-23 21:11:50 -0300

    Final-Melhorado

--- javascript/javascritp.js
+++ javascript/javascritp.js
@@ -1,4 +1,30 @@
 function formulario() {
+if(document.getElementById("nome").value == ""){
+    alert("Preencha o nome"); 
+    return false;
+    }
+
+if(document.getElementById("sexo").value == ""){
+    alert("Preencha o sexo"); 
+    return false;
+    }
+
+if(document.getElementById("data").value == ""){
+    alert("Preencha a data"); 
+    return false;
+    }
+
+if(document.getElementById("endereco").value == ""){
+    alert("Preencha o endereço"); 
+    return false;
+    }
+
+if(document.getElementById("mae").value == ""){
+    alert("Preencha o nome da mãe"); 
+    return false;
+    }
+
+
   var nome = String(document.getElementById("nome").value);
   var sexo = String(document.getElementById("sexo").value);
   var data = String(document.getElementById("data").value);
